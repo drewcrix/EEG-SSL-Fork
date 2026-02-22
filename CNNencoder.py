129cb475ba7bf37d71d8f3ec8dc9a6eeaba22978
drewcrix <drewcrix@gmail.com>
2026-02-22 02:33:34 -0500

    Clean up CNNencoder and adjust dropout parameter
    Removed unused dataset loading and plotting code for EEG signals. Updated dropout parameter in block1 initialization.

--- CNNencoder.py
+++ CNNencoder.py
@@ -3,48 +3,14 @@ import torch
 import torch.nn as nn
 import warnings
 from matplotlib import pyplot as plt
-from moabb.datasets import PhysionetMI
 from typing import Sequence, Union
-
-warnings.filterwarnings("ignore")
-
-
-dataset = PhysionetMI()
-subject = 1
-run = 4
-
-
-raw = dataset._load_one_run(subject, run)
-
-print(raw.get_data().shape)
-print("Raw info:")
-
-data = raw.get_data() #this has shape (64, 20000) for 64 channels and 2minutes of data at 160Hz sampling rate
-
-
-print("Data shape:", data.shape) #data shape (channels, timepoints)
-time_elapsed = 1/raw.info['sfreq'] * data.shape[1] #in seconds
-print("Total time elasped in this test (s)", time_elapsed) 
-
-f = data[0]  # First channel
-y = np.arange(0, time_elapsed, 1/raw.info['sfreq'])  # Time vector
-
-
-
-plt.plot(y, f) #plotting the EEG signal from the first channel to visualize the input data for the CNN layer
-plt.title('EEG Signal from First Channel')
-plt.xlabel('Time (s)')
-plt.ylabel('Amplitude (µV)')
-plt.show()
-
-
 
 class Block(nn.Module):
     """
     EEGNet-like temporal block. Followed similar format but removed spatial filer from depthwise conv2d
         Conv1d -> BatchNorm1d -> PReLU -> AvgPool1d -> Dropout
     """
-    def __init__(self, in_ch: int, out_ch: int, kernel_size: int, pool_size: int, dropout: float = 0.5, stride: int = 1, padding: Union[int, str] = "same"):
+    def __init__(self, in_ch: int, out_ch: int, kernel_size: int, pool_size: int, dropout: float, stride: int = 1, padding: Union[int, str] = "same"):
         super().__init__()
         self.conv = nn.Conv1d(
             in_channels=in_ch,
@@ -89,7 +55,7 @@ class CNNEncoder(nn.Module):
         o2 = o1 * 2
         o3 = o2 * 2
 
-        self.block1 = Block(1,  o1, kernel_sizes[0], pool_sizes[0], dropout, stride, padding)
+        self.block1 = Block(1,  o1, kernel_sizes[0], pool_sizes[0], 0.2, stride, padding)
         self.block2 = Block(o1, o2, kernel_sizes[1], pool_sizes[1], dropout, stride, padding)
         self.block3 = Block(o2, o3, kernel_sizes[2], pool_sizes[2], dropout, stride, padding)
 
@@ -110,7 +76,7 @@ class CNNEncoder(nn.Module):
 
        # x = x.mean(dim=-1)   #averages the strength of the features over time.
 
-        _, F, Tp = x.shape()
+        _, F, Tp = x.shape
 
         # Back to per-electrode node embeddings: (B, C, F)
         x = x.reshape(B, C, F, Tp) #keeping the time dimension 
