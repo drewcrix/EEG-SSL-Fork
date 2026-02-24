b0d1d2c6a234c6d26921c9780310895431f1c959
drewcrix <drewcrix@gmail.com>
2026-02-24 16:20:48 -0500

    Refactor adjacency_bids and update return values

--- GNNencoder.py
+++ GNNencoder.py
@@ -19,15 +19,15 @@ from torch_geometric.utils import dense_to_sparse
 from torch_geometric.nn import SAGPooling
 from torch.nn import Parameter
 
-dataset = PhysionetMI() #working for physionetMT() must change for BIDS
-subject = 1
-hand_runs = [4, 8, 12]  # Runs corresponding to left and right hand movements
-feet_runs = [6, 10, 14]  # Runs corresponding to feet movements
-
 def adjacency_bids(dataset, subject): #set the montage for specific dataset and subject passed through here
-    json_path = f"EEG-SSL/data/on/{dataset}/sub-{int(subject):03d}/eeg/sub-{int(subject):03d}_task-eyesclosed_eeg.json"
+    base_dir = Path(__file__).parent
+    dataset_dir = os.path.join(base_dir, f"data/on/{dataset}")
+    subject_dir = os.path.join(dataset_dir, f"{subject}")
+                               
+    search_json = os.path.join(subject_dir, "**", "*.json")
+    json_files = glob.glob(search_json, recursive=True)
     
-    with open(json_path, "r") as f:
+    with open(json_files, "r") as f:
         meta = json.load(f)
     
     scheme = (meta.get("EEGPlacementScheme") or "").lower()
@@ -44,68 +44,9 @@ def adjacency_bids(dataset, subject): #set the montage for specific dataset and 
     A_matrix = torch.tensor(A, dtype=torch.float32)  
     edge_index, edge_weight = dense_to_sparse(A_matrix)
 
-    return A, A_matrix, edge_index
+    return A_matrix, edge_index, edge_weight
 
-def _load_subject_raw(data, subj, handrun, feetrun, preload=True):
-    filenames = create_paths(subj, handrun, feetrun, data)
-    raws = {"hand": [], "feet": []}
-    raw_fname_hand = filenames['hand']
-    raw_fname_feet = filenames['feet'] 
-    for runname in raw_fname_hand:
-        raws["hand"].append(raw_info(read_raw_edf(runname, preload=preload, verbose="ERROR")))
-    for runname in raw_fname_feet:
-        raws["feet"].append(raw_info(read_raw_edf(runname, preload=preload, verbose="ERROR")))
-    return raws
-
-def create_paths(subject, hand_runs, feet_runs, dataset):
-    base_dir = Path(__file__).parent #directory above the current file
-    subject_dir = os.path.join(base_dir, f"data/subject_{subject}") #created path for this subject's data
-    path_hands = os.path.join(subject_dir, "hand_runs") #created path for hand runs data
-    path_feet = os.path.join(subject_dir, "feet_runs") #created path for feet runs data
-
-    dataset._load_data(subject, feet_runs, path=path_feet) #loads the feet runs locally for this subject
-    dataset._load_data(subject, hand_runs, path=path_hands) #loads the hand runs locally for this subject
-
-    #make a list of each downloaded run filename for this subject
-    filenames = {
-        "hand": sorted([str(p) for p in Path(path_hands).rglob("*.edf")]),
-        "feet": sorted([str(p) for p in Path(path_feet).rglob("*.edf")]),
-    }
-
-    return filenames
-
-def raw_info(raw):
-    raw.rename_channels(lambda x: x.strip("."))
-    raw.rename_channels(lambda x: x.upper())
-    # fmt: off
-    renames = {
-        "AFZ": "AFz", "PZ": "Pz", "FPZ": "Fpz", "FCZ": "FCz", "FP1": "Fp1", "CZ": "Cz",
-        "OZ": "Oz", "POZ": "POz", "IZ": "Iz", "CPZ": "CPz", "FP2": "Fp2", "FZ": "Fz",
-    }
-    # fmt: on
-    raw.rename_channels(renames)
-    raw.set_montage(mne.channels.make_standard_montage("standard_1005"))
-    return raw
-
-subject_raws = _load_subject_raw(dataset, subject, hand_runs, feet_runs, preload=True) #loads the raw data for this subject by loading the downloaded .edf files
-
-raw = subject_raws['hand'][0] 
-data = raw.get_data() #this has shape (64, 20000) for 64 channels and 2minutes of data at 160Hz sampling rate
-
-
-def get_adjacency_matrix(raw, n_neighbors=8):
-    montage = raw.get_montage()
-    positions = montage.get_positions()
-    pos_2d = {ch_name: np.array(pos[:2]) for ch_name, pos in positions["ch_pos"].items()}
-    ch_names = list(pos_2d.keys())
-    coords_2d = np.array([pos_2d[ch] for ch in ch_names])
-    A = kneighbors_graph(coords_2d, n_neighbors=n_neighbors, mode='connectivity', include_self=False).toarray()
-    
-    A_matrix = torch.tensor(A, dtype=torch.float32)  
-    edge_index, edge_weight = dense_to_sparse(A_matrix) #getting the edge_index matrix of size (2, num_of_edges) and edge weight vector of size (num_of_edges)
-    
-    return A, edge_index, edge_weight 
-
+ #this has shape (64, 20000) for 64 channels and 2minutes of data at 160Hz sampling rate 
 
 class GCNEncoder(nn.Module):
     """
